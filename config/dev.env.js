e823da9cd74d69b1bb678350252b3663cf2b4bf1
likai_loveweb@163.com <likai360>
2019-05-07 21:00:30 +0800

    reset

--- config/dev.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	NODE_ENV: '"development"',
-	ENV_CONFIG: '"dev"',
-	BASE_API: '"http://api.zhibobao.com"',
-  // HOST:'dev_gonghui.zhibobao.com'
-}
--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"prod"',
-	BASE_API: '"http://api.zhibobao.com"',
-  // HOST:'gonghui.zhibobao.com'
-}
